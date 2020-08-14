2953cc5c84cd6b9c30128aa280b03da8dee01bc3
Ahmed Gamal <ahmedmgh67@gmail.com>
2020-08-15 00:48:57 +0200

    Change database creds

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    $con=mysqli_connect("localhost","root","","site");
+    $con=mysqli_connect("localhost","bla","bla","bla");
     mysqli_query($con,"SET NAMES 'utf8'");
     mysqli_query($con,"SET CHARACTER SER utf8");
 ?>
