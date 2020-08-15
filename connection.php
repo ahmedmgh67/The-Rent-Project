cc74094537957386987c37463715a4e344484ad5
Ahmed Gamal <ahmedmgh67@gmail.com>
2020-08-15 04:26:58 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    $con=mysqli_connect("localhost","bla","bla","bla");
+    $con=mysqli_connect("127.0.0.1","bla","bla","bla");
     mysqli_query($con,"SET NAMES 'utf8'");
     mysqli_query($con,"SET CHARACTER SER utf8");
 ?>
